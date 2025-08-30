69179608fca2183081b5d51250c762cdd8f04f64
NVD576 <duw3210@gmail.com>
2025-08-30 20:04:45 +0700

    first comment

---
+++ Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace XuLyChuoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtS1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtS2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtS3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btChen = new System.Windows.Forms.Button();
+            this.txtViTriChen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btLaychuoicon = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSokitu = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTuvitri = new System.Windows.Forms.TextBox();
+            this.btThaytheS2_s3 = new System.Windows.Forms.Button();
+            this.btDaoTu = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btChuanHoaChuoi = new System.Windows.Forms.Button();
+            this.btKhoiPhuc = new System.Windows.Forms.Button();
+            this.txtChuoiCon = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(968, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHƯƠNG TRÌNH MINH HỌA XỬ LÝ CHUỖI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chuỗi s1:";
+            // 
+            // txtS1
+            // 
+            this.txtS1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtS1.Location = new System.Drawing.Point(187, 139);
+            this.txtS1.Multiline = true;
+            this.txtS1.Name = "txtS1";
+            this.txtS1.Size = new System.Drawing.Size(687, 28);
+            this.txtS1.TabIndex = 2;
+            this.txtS1.Text = "Khoa   công   nghệ thông tin, đại học mở thành phố Hồ Chí Minh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(68, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Chuỗi s2:";
+            // 
+            // txtS2
+            // 
+            this.txtS2.Location = new System.Drawing.Point(187, 210);
+            this.txtS2.Multiline = true;
+            this.txtS2.Name = "txtS2";
+            this.txtS2.Size = new System.Drawing.Size(194, 29);
+            this.txtS2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(565, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Chuỗi s3:";
+            // 
+            // txtS3
+            // 
+            this.txtS3.Location = new System.Drawing.Point(684, 210);
+            this.txtS3.Multiline = true;
+            this.txtS3.Name = "txtS3";
+            this.txtS3.Size = new System.Drawing.Size(190, 29);
+            this.txtS3.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btChen);
+            this.groupBox1.Controls.Add(this.txtViTriChen);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(87, 325);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 79);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chèn chuỗi s2 vào s1";
+            // 
+            // btChen
+            // 
+            this.btChen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btChen.Location = new System.Drawing.Point(181, 39);
+            this.btChen.Name = "btChen";
+            this.btChen.Size = new System.Drawing.Size(62, 32);
+            this.btChen.TabIndex = 2;
+            this.btChen.Text = "Chèn";
+            this.btChen.UseVisualStyleBackColor = true;
+            this.btChen.Click += new System.EventHandler(this.btChen_Click);
+            // 
+            // txtViTriChen
+            // 
+            this.txtViTriChen.Location = new System.Drawing.Point(100, 43);
+            this.txtViTriChen.Multiline = true;
+            this.txtViTriChen.Name = "txtViTriChen";
+            this.txtViTriChen.Size = new System.Drawing.Size(65, 22);
+            this.txtViTriChen.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 22);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Vị trí chèn:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btLaychuoicon);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtSokitu);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtTuvitri);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(87, 459);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(465, 72);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lấy chuỗi con trong s1";
+            // 
+            // btLaychuoicon
+            // 
+            this.btLaychuoicon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLaychuoicon.Location = new System.Drawing.Point(330, 33);
+            this.btLaychuoicon.Name = "btLaychuoicon";
+            this.btLaychuoicon.Size = new System.Drawing.Size(128, 32);
+            this.btLaychuoicon.TabIndex = 2;
+            this.btLaychuoicon.Text = "Lấy chuỗi con";
+            this.btLaychuoicon.UseVisualStyleBackColor = true;
+            this.btLaychuoicon.Click += new System.EventHandler(this.btLaychuoicon_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(169, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 22);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Số kí tự:";
+            // 
+            // txtSokitu
+            // 
+            this.txtSokitu.Location = new System.Drawing.Point(245, 37);
+            this.txtSokitu.Multiline = true;
+            this.txtSokitu.Name = "txtSokitu";
+            this.txtSokitu.Size = new System.Drawing.Size(65, 22);
+            this.txtSokitu.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 22);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Từ vị trí:";
+            // 
+            // txtTuvitri
+            // 
+            this.txtTuvitri.Location = new System.Drawing.Point(82, 37);
+            this.txtTuvitri.Multiline = true;
+            this.txtTuvitri.Name = "txtTuvitri";
+            this.txtTuvitri.Size = new System.Drawing.Size(65, 22);
+            this.txtTuvitri.TabIndex = 1;
+            // 
+            // btThaytheS2_s3
+            // 
+            this.btThaytheS2_s3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThaytheS2_s3.Location = new System.Drawing.Point(536, 313);
+            this.btThaytheS2_s3.Name = "btThaytheS2_s3";
+            this.btThaytheS2_s3.Size = new System.Drawing.Size(169, 32);
+            this.btThaytheS2_s3.TabIndex = 2;
+            this.btThaytheS2_s3.Text = "Thay thế s2 bằng s3";
+            this.btThaytheS2_s3.UseVisualStyleBackColor = true;
+            this.btThaytheS2_s3.Click += new System.EventHandler(this.btThaytheS2_s3_Click);
+            // 
+            // btDaoTu
+            // 
+            this.btDaoTu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDaoTu.Location = new System.Drawing.Point(711, 313);
+            this.btDaoTu.Name = "btDaoTu";
+            this.btDaoTu.Size = new System.Drawing.Size(152, 32);
+            this.btDaoTu.TabIndex = 2;
+            this.btDaoTu.Text = "Đảo từ trong s1";
+            this.btDaoTu.UseVisualStyleBackColor = true;
+            this.btDaoTu.Click += new System.EventHandler(this.btDaoTu_Click);
+            // 
+            // btXoa
+            // 
+            this.btXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(536, 372);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(169, 32);
+            this.btXoa.TabIndex = 2;
+            this.btXoa.Text = "Xóa s2 trong s1";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btChuanHoaChuoi
+            // 
+            this.btChuanHoaChuoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btChuanHoaChuoi.Location = new System.Drawing.Point(711, 372);
+            this.btChuanHoaChuoi.Name = "btChuanHoaChuoi";
+            this.btChuanHoaChuoi.Size = new System.Drawing.Size(152, 32);
+            this.btChuanHoaChuoi.TabIndex = 2;
+            this.btChuanHoaChuoi.Text = "Chuẩn hóa chuỗi";
+            this.btChuanHoaChuoi.UseVisualStyleBackColor = true;
+            this.btChuanHoaChuoi.Click += new System.EventHandler(this.btChuanHoaChuoi_Click);
+            // 
+            // btKhoiPhuc
+            // 
+            this.btKhoiPhuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btKhoiPhuc.Location = new System.Drawing.Point(628, 459);
+            this.btKhoiPhuc.Name = "btKhoiPhuc";
+            this.btKhoiPhuc.Size = new System.Drawing.Size(152, 32);
+            this.btKhoiPhuc.TabIndex = 2;
+            this.btKhoiPhuc.Text = "Khôi Phục";
+            this.btKhoiPhuc.UseVisualStyleBackColor = true;
+            this.btKhoiPhuc.Click += new System.EventHandler(this.btKhoiPhuc_Click);
+            // 
+            // txtChuoiCon
+            // 
+            this.txtChuoiCon.BackColor = System.Drawing.Color.White;
+            this.txtChuoiCon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChuoiCon.Location = new System.Drawing.Point(81, 552);
+            this.txtChuoiCon.Name = "txtChuoiCon";
+            this.txtChuoiCon.Size = new System.Drawing.Size(300, 23);
+            this.txtChuoiCon.TabIndex = 5;
+            this.txtChuoiCon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(968, 609);
+            this.Controls.Add(this.txtChuoiCon);
+            this.Controls.Add(this.btDaoTu);
+            this.Controls.Add(this.btChuanHoaChuoi);
+            this.Controls.Add(this.btKhoiPhuc);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.btThaytheS2_s3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtS3);
+            this.Controls.Add(this.txtS2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtS1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Xu ly chuoi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtS1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtS2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtS3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btChen;
+        private System.Windows.Forms.TextBox txtViTriChen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btLaychuoicon;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSokitu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTuvitri;
+        private System.Windows.Forms.Button btThaytheS2_s3;
+        private System.Windows.Forms.Button btDaoTu;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btChuanHoaChuoi;
+        private System.Windows.Forms.Button btKhoiPhuc;
+        private System.Windows.Forms.Label txtChuoiCon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XuLyChuoi
+{
+    public partial class Form1 : Form
+    {
+
+        string chuoiBanDau;
+
+        public Form1()
+        {
+            InitializeComponent();
+            chuoiBanDau = txtS1.Text;
+        }
+        
+
+        private void btChen_Click(object sender, EventArgs e)
+        {
+            txtS1.Text = txtS1.Text.Insert(int.Parse(txtViTriChen.Text),txtS2.Text);
+        }
+
+        private void btKhoiPhuc_Click(object sender, EventArgs e)
+        {
+            txtS1.Text = chuoiBanDau;
+        }
+
+        private void btThaytheS2_s3_Click(object sender, EventArgs e)
+        {
+            txtS1.Text=txtS1.Text.Replace(txtS2.Text,txtS3.Text);
+        }
+
+        private void btDaoTu_Click(object sender, EventArgs e)
+        {
+                string[] s = txtS1.Text.Split(' ');
+                Array.Reverse(s);
+                txtS1.Text=string.Join(" ", s);
+
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            int index = txtS1.Text.IndexOf(txtS2.Text);
+            if (index != -1)
+                txtS1.Text = txtS1.Text.Remove(index,txtS2.Text.Length);
+        }
+
+        private void btLaychuoicon_Click(object sender, EventArgs e)
+        {
+            int index = int.Parse(txtTuvitri.Text);
+            int count=int.Parse(txtSokitu.Text);
+            txtChuoiCon.Text = txtS1.Text.Substring(index, count);
+        }
+
+        private void btChuanHoaChuoi_Click(object sender, EventArgs e)
+        {
+            txtS1.Text = txtS1.Text.Trim();
+            txtS1.Text= Regex.Replace(txtS1.Text, @"\s+", " ");
+            string[] s = txtS1.Text.Split(' ');
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Length > 0)
+                {
+                    s[i] = char.ToUpper(s[i][0]) + s[i].Substring(1).ToLower();
+                }
+            }
+
+            txtS1.Text = string.Join(" ", s);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
